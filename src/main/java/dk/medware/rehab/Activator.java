4132c1d313204f6c3eb35ce5d9f4b4a777360884
Jacob Glasdam <jacob@medware.dk>
2014-10-27 09:43:38 +0100

    Start

---
+++ src/main/java/dk/medware/rehab/Activator.java
@@ -0,0 +1,33 @@
+package dk.medware.rehab;
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.ontology.device.StatusValue;
+
+public class Activator implements BundleActivator {
+	public static BundleContext osgiContext = null;
+	public static ModuleContext context = null;
+
+	public static SCallee scallee = null;
+	public static CSubscriber csubscriber = null;
+	public static CPublisher cpublisher = null;
+
+	public void start(BundleContext bcontext) throws Exception {
+        	Activator.osgiContext = bcontext;
+        	Activator.context = uAALBundleContainer.THE_CONTAINER
+        		.registerModule(new Object[] { bcontext });
+        	scallee = new SCallee(context);
+        	csubscriber = new CSubscriber(context);
+        	cpublisher = new CPublisher(context);
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+        	scallee.close();
+        	csubscriber.close();
+        	cpublisher.close();
+	}
+
+}
---
+++ src/main/java/dk/medware/rehab/SCalleeProvidedService.java
@@ -0,0 +1,83 @@
+package dk.medware.rehab;
+
+
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.middleware.owl.SimpleOntology;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.rdf.ResourceFactory;
+import org.universAAL.middleware.rdf.TypeMapper;
+import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.ontology.phThing.DeviceService;
+import org.universAAL.ontology.rehabontology.ExerciseAnalyser;
+import org.universAAL.ontology.rehabontology.ExerciseResults;
+import org.universAAL.ontology.device.SwitchController;
+
+/* -Example- This service example provides Device Services*/
+public class SCalleeProvidedService extends DeviceService {
+
+	/*
+	 * -Example- this namespace can be reused in many parts of the code, but not
+	 * all of them
+	 */
+	protected static final String SERVICE_OWN_NAMESPACE = "http://your.ontology.URL.com/YourServerDomainOntology.owl#";
+	// TODO: Change Namespace
+	public static final String MY_URI = SERVICE_OWN_NAMESPACE
+			+ "OnOffActuatorTemplateService";
+	protected static final String SERVICE_GET_EXERCISE_SUGGESTION_URI = SERVICE_OWN_NAMESPACE
+			+ "getStatus";
+	protected static final String SERVICE_SET_STATUS_URI = SERVICE_OWN_NAMESPACE
+			+ "setStatus";
+	protected static final String OUTPUT_STATUS = SERVICE_OWN_NAMESPACE
+			+ "outputStatus";
+	protected static final String INPUT_STATUS = SERVICE_OWN_NAMESPACE
+			+ "inputStatus";
+	protected static final String INPUT_DEVICE = SERVICE_OWN_NAMESPACE
+			+ "inputDevice";
+
+	/* INPUT
+	 *  [ [result (integer), Timestamp (long) ] ]
+	 * OUTPUT
+	 * [ suggestion (integer), slope (float), baseline (integer) ]
+	 */
+	/* -Example- This registers three profiles */
+	public static ServiceProfile[] profiles = new ServiceProfile[1];
+
+	static {
+		/*
+		 * -Example- This piece of code tells ontology management that these
+		 * provided services extend the DeviceService ontology, without having
+		 * to code a full Ontology class
+		 */
+		OntologyManagement.getInstance().register(
+				Activator.context,
+				new SimpleOntology(MY_URI, ExerciseAnalyser.MY_URI,
+						new ResourceFactory() {
+							public Resource createInstance(String classURI,
+									String instanceURI, int factoryIndex) {
+								return new SCalleeProvidedService(instanceURI);
+							}
+						}));
+
+		// Declaration of first profile. In: SwitchController; Out: StatusValue
+		SCalleeProvidedService getExerciseSuggestion = new SCalleeProvidedService(
+				SERVICE_GET_EXERCISE_SUGGESTION_URI);
+		getExerciseSuggestion.addInputWithAddEffect(INPUT_DEVICE, ExerciseResults.MY_URI, 1, -1, new String []{ ExerciseAnalyser.PROP_EXERCISE_RESULTS });
+/*		getExerciseSuggestion.addOutput(OUTPUT_STATUS,
+				TypeMapper.getDatatypeURI(Boolean.class), 1, 1, new String[] {
+						DeviceService.PROP_CONTROLS,
+						SwitchController.PROP_HAS_VALUE });
+						*/
+//		profiles[0].getTheService().addInstanceLevelRestriction(r, new String[] { DeviceService.PROP_CONTROLS });
+		profiles[0] = getExerciseSuggestion.getProfile();
+	}
+
+	protected SCalleeProvidedService(String uri) {
+		super(uri);
+	}
+	
+	 public String getClassURI() {
+		 return MY_URI;
+	 }
+
+}
